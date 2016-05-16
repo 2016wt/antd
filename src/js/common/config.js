020bb5fe2deea704da53e49f1183dd3ec5ca5b0f
niyingfeng <yingfeng.ni@gmail.com>
2016-05-16 14:32:22 +0800

    change

---
+++ src/js/common/config.js
@@ -0,0 +1,11 @@
+const Config = {
+    asideList : [
+        tab1: 'tab1',
+        tab2: 'tab2',
+        tab3: 'tab3',
+        tab4: 'tab4',
+        tab5: 'tab5'
+    ]
+}
+
+export default Config;
