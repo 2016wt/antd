56616654bec6b99cb203232c810294692ca7c716
niyingfeng <yingfeng.ni@gmail.com>
2016-06-02 10:32:43 +0800

    change

---
+++ src/js/component/Feature1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Feature1 = React.createClass({
+    render: function() {
+        return  <div>
+                    <h4>Feature1</h4>
+                </div>
+    }
+});
+
+export default Feature1;
---
+++ src/js/component/Feature2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Feature2 = React.createClass({
+    render: function() {
+        return  <div>
+                    <h4>Feature2</h4>
+                </div>
+    }
+});
+
+export default Feature2;
