4a16dadac1cc9ef5c6d170ae923941c76b2cdabd
niyingfeng <yingfeng.ni@gmail.com>
2016-07-12 15:24:37 +0800

    editor finished

--- src/js/feature/Feature3-1.jsx
+++ src/js/feature/Feature3-1.jsx
@@ -1,28 +1,42 @@
 // 含有可操作 table 栏的数据展示
 import React from 'react';
-import TinyMCE from 'react-tinymce';
 
 import Immutable from 'immutable';
+import TinyMCE from 'react-tinymce';
 //https://github.com/ded/reqwest
 import Reqwest from 'reqwest';
 
 const Feature = React.createClass({
-    getDefaultProps: function(){
+    getInitialState: function(){
         return {
+            value: ''
         }
     },
     render: function() {
-        return  <TinyMCE
-                    content="<p>This is the initial content of the editor</p>"
-                    config={{
-                      plugins: 'link image code',
-                      toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
-                    }}
-                    onChange={this.handleEditorChange}/>
+        let config = {
+            content: "<p>This is the initial content of the editor</p>",
+            config: {
+                height: '250',
+                plugins: [
+                    "advlist autolink lists charmap print preview hr anchor pagebreak spellchecker",
+                    "searchreplace wordcount visualblocks visualchars fullscreen insertdatetime  nonbreaking",
+                    "save table contextmenu directionality emoticons paste textcolor"
+                ],
+                toolbar: "insertfile undo redo | styleselect fontselect fontsizeselect| bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      | print preview fullpage | forecolor backcolor", 
+            },
+            onChange: this.handleEditorChange
+        }
+        return  <div>
+            <p style={{margin:'10px'}}>html文本：{this.state.value}</p>
+            <TinyMCE className="editor" {...config} />
+        </div>
     },
 
     handleEditorChange(e) {
-        console.log('Content was updated:', e.target.getContent());
+        window.e = e;
+        this.setState({
+            value: e.target.getContent()
+        });
     },
 });
 
