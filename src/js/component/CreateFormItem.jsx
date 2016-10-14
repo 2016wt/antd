56ac01a9e05321dcbcd5f848d2a5da64e7a7edc8
niyingfeng <yingfeng.ni@gmail.com>
2016-10-14 17:16:24 +0800

    fixed bug & add cascader form item

--- src/js/component/CreateFormItem.jsx
+++ src/js/component/CreateFormItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Select, Input, Button, Icon , DatePicker, TimePicker, Radio, Switch} from 'antd';
+import { Form, Select, Input, Button, Icon , DatePicker, TimePicker, Radio, Switch, Cascader} from 'antd';
 import { Upload, Modal, message } from 'antd';
 
 import BDUploader from './BDUploader';
@@ -50,13 +50,22 @@ let CFormItem = React.createClass({
                             label={item.label}
                             key={item.name}
                             {...formItemLayout}>
-                            <Select  {...getFieldProps(item.name, { initialValue: defaultValue })} style={{ width: item.width }}>
+                            <Select  {...getFieldProps(item.name, { initialValue: defaultValue })} style={{ width: item.width }} onChange={item.onChange || function(){}}>
                                 {
                                     item.options.map(function(item){
-                                        return <Option key={item.value} value={item.value}>{item.text}</Option>
+                                        return <Option key={item.value} value={item.value}>{item.text || item.value}</Option>
                                     })
                                 }
                             </Select>
+                        </FormItem>
+                break;
+
+            case 'cascader':
+                return <FormItem
+                            label={item.label}
+                            key={item.name}
+                            {...formItemLayout}>
+                            <Cascader  {...getFieldProps(item.name, { initialValue: defaultValue })} options={item.options} style={{ width: item.width }} changeOnSelect/>
                         </FormItem>
                 break;
 
@@ -68,7 +77,7 @@ let CFormItem = React.createClass({
                             <RadioGroup {...getFieldProps(item.name, { initialValue: defaultValue })}>
                                 {
                                     item.options.map(function(item){
-                                        return <Radio key={item.value} value={item.value}>{item.text}</Radio>
+                                        return <Radio key={item.value} value={item.value}>{item.text || item.value}</Radio>
                                     })
                                 }
                             </RadioGroup>
@@ -90,9 +99,10 @@ let CFormItem = React.createClass({
                             label={item.label}
                             key={item.name}
                             {...formItemLayout}>
-                            <Input
-                            {...getFieldProps(item.name, { initialValue:defaultValue})} /> 
                             <img className="uploadImg" src={defaultValue} />
+                            <Input
+                                {...getFieldProps(item.name, { initialValue:defaultValue})}
+                                onChange={this.changeImgUrl} /> 
                             <BDUploader success={this.uploadSuccess} />
                         </FormItem>
 
@@ -107,6 +117,12 @@ let CFormItem = React.createClass({
         console.log(url)
         this.setState({
             img_url: url
+        })
+    },
+    changeImgUrl: function(e){
+        console.log(e.target.value)
+        this.setState({
+            img_url: e.target.value
         })
     }
 });
